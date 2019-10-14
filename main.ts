1505ec9cc4242b517a10c4196764c8ef0038929a
sneklord <44676599+sneklord@users.noreply.github.com>
2019-10-14 11:32:35 -0700

    bye

--- main.ts
+++ main.ts
@@ -3,5 +3,6 @@ brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
     brick.setStatusLight(StatusLight.GreenFlash)
     brick.showString("Hello world", 1)
     brick.showString("Everything is awesome", 2);
+    brick.showString("Goodbye world", 1)
 
 })
